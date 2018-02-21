9934932428cc8d2dc74a8057d8e5ac72131a42ce
mattskelley <matthew.skelley@tjm.com.au>
2018-02-21 13:02:17 +1000

    3.1 Creating Tasks

--- ParallelProgramming/Program.cs
+++ ParallelProgramming/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace ParallelProgramming
 {
-    class Program
+    internal static class Program
     {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i -- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+
         static void Main(string[] args)
         {
+            // Create a task
+            Task.Factory.StartNew(() => Write('.'));    // Creating and starting at the same time
+            // Another way of making a task
+            //Make an instance
+            var t = new Task(() => Write('?')); // We havent started yet
+            // There is a difference between these implementations 
+            t.Start();
+
+            Write('-'); // On the main thread
+
+
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
         }
     }
 }
